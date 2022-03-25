fa405a565147177e235ff80b360036640adddeb9
Kristof Toth <toth.kristof111@gmail.com>
2022-03-25 19:00:30 +0100

    functions for generating random numbers/vectors

---
+++ 3DMath/Random.h
@@ -0,0 +1,32 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <random>
+#include "Vector.h"
+
+namespace math
+{
+	template<typename T>
+	T RandomReal(T min = T(0.0), T max = T(1.0))
+	{
+		static std::uniform_real_distribution<T> distribution(min, max);
+		static std::mt19937 generator;
+		return distribution(generator);
+	}
+
+
+	template<typename T>
+	vec<3,T> RandomInUnitSphere()
+	{
+		while(true)
+		{
+			vec<3,T> res(RandomReal<T>(), RandomReal<T>(),RandomReal<T>());
+			if(math::lengthSq(res) >= T(1))
+				continue;
+			else
+				return res;
+		}
+	}
+
+}
+#endif
